5234f5bb4a97e36bd7051e04f21b9586795fec79
Saubhagya Srivastava <saubhagya@Saubhagyas-MacBook-Air.local>
2022-04-16 02:46:17 +0530

    logo name change

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         <div className="sidebar">
             <div className="top">
                 <Link to="/" style={{textDecoration:"none"}}>
-                    <span className="logo">logo</span>
+                    <span className="logo">adminDash</span>
                 </Link>   
             </div>
             <hr />
